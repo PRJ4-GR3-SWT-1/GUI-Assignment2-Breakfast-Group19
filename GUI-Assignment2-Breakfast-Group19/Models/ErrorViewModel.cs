d79df54da7a28204cf618bba8e647c85deb37278
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-04-19 08:33:27 +0200

    Add project files.

---
+++ GUI-Assignment2-Breakfast-Group19/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GUI_Assignment2_Breakfast_Group19.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
