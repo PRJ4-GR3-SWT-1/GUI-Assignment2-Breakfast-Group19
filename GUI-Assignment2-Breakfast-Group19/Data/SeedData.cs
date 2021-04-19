8c0793479c15b275d5755efffaeede3446076303
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-04-19 12:32:53 +0200

    fix

--- GUI-Assignment2-Breakfast-Group19/Data/SeedData.cs
+++ GUI-Assignment2-Breakfast-Group19/Data/SeedData.cs
@@ -16,12 +16,7 @@ namespace GUI_Assignment2_Breakfast_Group19.Data
             if (r == null)
             {
                 Seed7Day(dbContext);
-                //SeedSecondDay(dbContext);
-                //SeedThirdDay(dbContext);
-                //SeedFourthDay(dbContext);
-                //SeedFifthDay(dbContext);
-                //SeedSixthDay(dbContext);
-                //SeedSeventhDay(dbContext);
+              
             }
         }
 
@@ -43,100 +38,22 @@ namespace GUI_Assignment2_Breakfast_Group19.Data
             for (int day = 0; day < 6; day++)
             {
                 
-           
-            BreakfastReservations br = new BreakfastReservations(DateTime.Today.AddDays(day));
-            for (int i = 1;i < 5; i++)
-            {
-                Room r = SeedRoomRandomAdultAndChildren(i,dbContext);
+                BreakfastReservations br = new BreakfastReservations(DateTime.Today.AddDays(day));
+                for (int i = 1;i < 5; i++)
+                {
+                    Room r = SeedRoomRandomAdultAndChildren(i,dbContext);
                     if ((i % 2) == 1)
                     {
 
                         br.BreakfastReservationList.Add(r);
 
+                    }
                 }
-            }
-            dbContext.BreakfastReservations.Add(br);
+                dbContext.BreakfastReservations.Add(br);
             }
 
             dbContext.SaveChanges();
         }
-
-        //private void SeedSecondDay(ApplicationDbContext dbContext)
-        //{
-        //    for (int i = 1; i < 5; i++)
-        //    {
-        //        Room r = SeedRoomRandomAdultAndChildren(i, dbContext);
-        //        if ((i % 2) == 1)
-        //        {
-        //            BreakfastReservations br = new BreakfastReservations(r, DateTime.Today.AddDays(1));
-        //            dbContext.BreakfastReservations.Add(br);
-        //        }
-        //    }
-        //}
-
-        //private void SeedThirdDay(ApplicationDbContext dbContext)
-        //{
-        //    for (int i = 1; i < 5; i++)
-        //    {
-        //        Room r = SeedRoomRandomAdultAndChildren(i, dbContext);
-        //        if ((i % 2) == 1)
-        //        {
-        //            BreakfastReservations br = new BreakfastReservations(r, DateTime.Today.AddDays(2));
-        //            dbContext.BreakfastReservations.Add(br);
-        //        }
-        //    }
-        //}
-
-        //private void SeedFourthDay(ApplicationDbContext dbContext)
-        //{
-        //    for (int i = 1; i < 5; i++)
-        //    {
-        //        Room r = SeedRoomRandomAdultAndChildren(i, dbContext);
-        //        if ((i % 2) == 1)
-        //        {
-        //            BreakfastReservations br = new BreakfastReservations(r, DateTime.Today.AddDays(3));
-        //            dbContext.BreakfastReservations.Add(br);
-        //        }
-        //    }
-        //}
-
-        //private void SeedFifthDay(ApplicationDbContext dbContext)
-        //{
-        //    for (int i = 1; i < 5; i++)
-        //    {
-        //        Room r = SeedRoomRandomAdultAndChildren(i, dbContext);
-        //        if ((i % 2) == 1)
-        //        {
-        //            BreakfastReservations br = new BreakfastReservations(r, DateTime.Today.AddDays(4));
-        //            dbContext.BreakfastReservations.Add(br);
-        //        }
-        //    }
-        //}
-
-        //private void SeedSixthDay(ApplicationDbContext dbContext)
-        //{
-        //    for (int i = 1; i < 5; i++)
-        //    {
-        //        Room r = SeedRoomRandomAdultAndChildren(i, dbContext);
-        //        if ((i % 2) == 1)
-        //        {
-        //            BreakfastReservations br = new BreakfastReservations(r, DateTime.Today.AddDays(5));
-        //            dbContext.BreakfastReservations.Add(br);
-        //        }
-        //    }
-        //}
-
-        //private void SeedSeventhDay(ApplicationDbContext dbContext)
-        //{
-        //    for (int i = 1; i < 5; i++)
-        //    {
-        //        Room r = SeedRoomRandomAdultAndChildren(i, dbContext);
-        //        if ((i % 2) == 1)
-        //        {
-        //            BreakfastReservations br = new BreakfastReservations(r, DateTime.Today.AddDays(6));
-        //            dbContext.BreakfastReservations.Add(br);
-        //        }
-        //    }
-        //}
+        
     }
 }
