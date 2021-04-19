c57a6eee53dba4935ea678e9189cd5c37832c6bf
Laurits H.G <201905745@post.au.dk>
2021-04-19 09:12:15 +0200

    Made ArrivalsAtBreakfastId to public

--- GUI-Assignment2-Breakfast-Group19/Models/ArrivalsAtBreakfast.cs
+++ GUI-Assignment2-Breakfast-Group19/Models/ArrivalsAtBreakfast.cs
@@ -7,7 +7,7 @@ namespace GUI_Assignment2_Breakfast_Group19.Models
 {
     public class ArrivalsAtBreakfast
     {
-        private int ArrivalsAtBreakfastId { get; set; }
+        public int ArrivalsAtBreakfastId { get; set; }
         public DateTime Date { get; set; }
         public List<Room> BreakfastAttendees { get; set; }
     }
