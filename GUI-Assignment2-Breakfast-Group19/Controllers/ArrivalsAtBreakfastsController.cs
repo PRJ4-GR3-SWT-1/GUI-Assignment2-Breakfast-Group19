d99fb8942b0a7c41dab26ace6b89e5ebcd573fc3
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-04-20 17:07:58 +0200

    arrivals now shows multiple days

--- GUI-Assignment2-Breakfast-Group19/Controllers/ArrivalsAtBreakfastsController.cs
+++ GUI-Assignment2-Breakfast-Group19/Controllers/ArrivalsAtBreakfastsController.cs
@@ -24,6 +24,7 @@ namespace GUI_Assignment2_Breakfast_Group19.Controllers
         public async Task<IActionResult> Index()
         {
             var sd = new SeedData(_context);
+            //Check if today exists:
             var arrivals = _context.ArrivalsAtBreakfast
                 .Include(a => a.BreakfastAttendees)
                 .SingleOrDefault(a => a.Date.Date == DateTime.Today.Date);
@@ -33,6 +34,26 @@ namespace GUI_Assignment2_Breakfast_Group19.Controllers
                 _context.ArrivalsAtBreakfast.Add(arrivals);
                 _context.SaveChanges();
             }
+
+            var arrivalList = _context.ArrivalsAtBreakfast.ToList();
+
+            return View(arrivalList);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var arrivals = _context.ArrivalsAtBreakfast
+                .Include(a => a.BreakfastAttendees)
+                .SingleOrDefault(a => a.ArrivalsAtBreakfastId==id);
+            if (arrivals == null)
+            {
+                return NotFound();
+            }
+
             //Get reservations:
             var res = _context.BreakfastReservations
                 .Include(r => r.BreakfastReservationList)
@@ -41,11 +62,31 @@ namespace GUI_Assignment2_Breakfast_Group19.Controllers
             ArrivalsExtended arrivalsExtended = new ArrivalsExtended(arrivals);
             if (resNumber != null)
             {
-                    arrivalsExtended.AdultReservations = resNumber[0];
-                    arrivalsExtended.ChildReservations = resNumber[1];
+                arrivalsExtended.AdultReservations = resNumber[0];
+                arrivalsExtended.ChildReservations = resNumber[1];
             }
 
             return View(arrivalsExtended);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BreakfastReservations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BreakfastReservationsId,Date")] ArrivalsAtBreakfast arrivalsAtBreakfast)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(arrivalsAtBreakfast);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
